5a6efc0332582239ad9a0935e9fb97fb17411670
Dmitry Fleish <DXF0530@corp.aeroscout.com>
2019-02-22 17:05:24 +0200

    Finish with interceptor

--- src/app/config-interceptor-/config-interceptor-.service.ts
+++ src/app/config-interceptor-/config-interceptor-.service.ts
@@ -1,14 +1,42 @@
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError  } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ConfigInterceptorService implements HttpInterceptor   {
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    throw new Error("Method not implemented.");
-  }
+export class ConfigInterceptorService implements HttpInterceptor {
 
-  constructor() { }
+  constructor(public errorDialogService: ErrorDialogService) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token: string = localStorage.getItem('token');
+    if (token) {
+      request = request.clone({ headers: request.headers.set('Authorization', 'Bearer ' + token) });
+    }
+
+    if (!request.headers.has('Content-Type')) {
+      request = request.clone({ headers: request.headers.set('Content-Type', 'application/json') });
+    }
+
+    request = request.clone({ headers: request.headers.set('Accept', 'application/json') });
+
+    return next.handle(request).pipe(
+      map((event: HttpEvent<any>) => {
+        if (event instanceof HttpResponse) {
+          console.log('event--->>>', event);
+        }
+        return event;
+      }),
+      catchError((error: HttpErrorResponse) => {
+        let data = {};
+        data = {
+            reason: error && error.error.reason ? error.error.reason : '',
+            status: error.status
+        };
+        this.errorDialogService.openDialog(data);
+        return throwError(error);
+    }));
+  }
 }
