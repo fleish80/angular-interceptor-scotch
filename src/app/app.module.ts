7300d7e2eb784b6e01b59684cc08c577bd9569bf
Dmitry Fleish <DXF0530@corp.aeroscout.com>
2019-02-24 22:35:57 +0200

    Finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { MatDialogModule } from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ErrorDialogComponent } from './error-dialog/error-dialog.component';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ConfigInterceptorService } from './config-interceptor-/config-interceptor-.service';
+import { ErrorDialogComponent } from './error-dialog/error-dialog.component';
+
 
 @NgModule({
   declarations: [
@@ -16,11 +17,14 @@ import { ConfigInterceptorService } from './config-interceptor-/config-intercept
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatDialogModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: ConfigInterceptorService, multi: true }
   ],
+  entryComponents: [ErrorDialogComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
